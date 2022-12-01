db735277c4139f7119964e6a9b0396adc6812804
Mihail Sevagin <47269465+Reckstorm@users.noreply.github.com>
2022-12-01 23:36:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string path = "C:\\Users\\Admin\\Downloads\\Telegram Desktop";
+string pathNew = "E:\\Files";
+
+List<string> types = new List<string>() { ".jpg", ".mp3", ".mp4", ".ogg"};
+
+DirectoryInfo DirectoryInfo = new DirectoryInfo(path);
+
+foreach (var item in DirectoryInfo.GetFiles())
+{
+    string temp;
+    try
+    {
+        if (types.Contains(item.Extension))
+        {
+            temp = $"{pathNew}\\{item.Extension}\\";
+            Directory.CreateDirectory(temp);
+            item.MoveTo(temp + item.Name);
+        }
+        else
+        {
+            temp = $"{pathNew}\\Misc\\";
+            Directory.CreateDirectory(temp);
+            item.MoveTo(temp + item.Name);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Run program as an admin");
+        Console.ReadKey(true);
+        break;
+    }
+}
